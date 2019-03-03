e2a53da6d01d09eabb206723a7a1e7ae51c8ac9f
gpid007 <heine.gregor@gmail.com>
2019-03-04 00:00:10 +0100

    light vimrc

--- light_install.sh
+++ light_install.sh
@@ -6,7 +6,6 @@ VIM=~/vim
 VDOT=~/.vim
 GIT_BASH=~/.bash-git-prompt
 DIR_ARR=($VIM $VDOT $GIT_BASH)
-PWD=$HOME/student-aws
 # PWD=$(basename $0)
 
 echo "# ========== OS-RELEASE INSTALL ========== #"
@@ -54,7 +53,7 @@ echo "# ========== VIM ========== #"
 # sudo pip3 install yapf pycodestyle
  
 # ---------- VIMRC ---------- #
-cp $PWD/vimrc ~/.vimrc
+cp ~/student-aws/vimrc ~/.vimrc
  
 # # ---------- BUNDLE ---------- #
 # vim -c 'BundleInstall!' -c 'qa!'
@@ -84,7 +83,7 @@ bind M set -g mode-mouse off \; set -g mouse-resize-pane off \; set -g mouse-sel
 EOF
 fi
 # ---------- .TMUX.CONF COPY  ----------
-cat $PWD/tmux.conf >> ~/.tmux.conf
+cat ~/student-aws/tmux.conf >> ~/.tmux.conf
 
 
 echo "# ========== GIT BASH ========= #"
